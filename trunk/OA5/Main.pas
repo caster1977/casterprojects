8ce283754e4f75d7b989e438e3b0efbd3eadc16f
vlad_dracula@tut.by <caster1977@yandex.ru>
2010-12-09 22:45:21 +0000

    Добавлены некоторые действия, иконки к действиям и частично создано главное меню.

--- trunk/OA5/Main.pas
+++ trunk/OA5/Main.pas
@@ -4,7 +4,8 @@ interface
 
 uses
   Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
-  Dialogs, Menus, ComCtrls, PlatformDefaultStyleActnCtrls, ActnList, ActnMan;
+  Dialogs, Menus, ComCtrls, PlatformDefaultStyleActnCtrls, ActnList, ActnMan,
+  ImgList;
 
 type
   TMainForm = class(TForm)
@@ -15,6 +16,30 @@ type
     N3: TMenuItem;
     N4: TMenuItem;
     ActionManager1: TActionManager;
+    Action_Quit: TAction;
+    Action_About: TAction;
+    Action_Help: TAction;
+    Action_Configuration: TAction;
+    Action_Accounts: TAction;
+    Action_Reports: TAction;
+    Action_Logon: TAction;
+    Action_Logout: TAction;
+    N5: TMenuItem;
+    N6: TMenuItem;
+    O1: TMenuItem;
+    N7: TMenuItem;
+    N8: TMenuItem;
+    N9: TMenuItem;
+    N10: TMenuItem;
+    N11: TMenuItem;
+    N12: TMenuItem;
+    N13: TMenuItem;
+    N14: TMenuItem;
+    N15: TMenuItem;
+    miStatusBar: TMenuItem;
+    ilSmallImages: TImageList;
+    ilBigImages: TImageList;
+    procedure Action_QuitExecute(Sender: TObject);
   private
     { Private declarations }
   public
@@ -27,5 +52,14 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TMainForm.Action_QuitExecute(Sender: TObject);
+const
+  LogGroupGUID: string='{3EB0BDC1-4DB7-44CF-AAE6-5303BB2B41D4}';
+begin
+//  ProcedureHeader('Процедура-обработчик действия "'+Action_Quit.Caption+'"', LogGroupGUID);
+  Close;
+//  ProcedureFooter(LogGroupGUID);
+end;
 
 end.
