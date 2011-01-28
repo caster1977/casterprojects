2e139ab93de42acf68e419db7fa100e722e25013
vlad_dracula@tut.by <caster1977@yandex.ru>
2011-01-28 23:31:14 +0000

    Share project "Lesson04HomeTask04" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson12" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson10HomeTask04" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson06HomeTask04_1" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson07" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson10HomeTask05" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson05HomeTask14" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson12HomeTask01" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson10" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson09" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson08HomeTask03" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson06HomeTask04_2" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson02" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson08HomeTask02" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson03HomeTask04" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson08HomeTask01" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson03" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson09HomeTask_1" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson10HomeTask01" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson06" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson10HomeTask02" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson05" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson02HomeTask04" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson01HomeTask04" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson07HomeTask04" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson10HomeTask03" into "https://casterprojects.googlecode.com/svn/JavaSE"

---
+++ JavaSE/trunk/Lesson03/MyPackage/Main.java
@@ -0,0 +1,10 @@
+package MyPackage;
+
+public class Main 
+{
+	public static void main(String[] args) 
+	{
+		ClassDemo classDemo1 = new ClassDemo(); 
+		ClassDemo classDemo2 = new ClassDemo(5);
+	}
+}
