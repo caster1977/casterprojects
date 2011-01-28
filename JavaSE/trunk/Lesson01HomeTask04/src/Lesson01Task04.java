72e60284ee6c51135ca8196ab2a9a4a1f980aae9
vlad_dracula@tut.by <vlad_dracula@tut.by@0ce95480-0193-c130-4e2c-61057acf6c8b>
2011-01-28 23:33:00 +0000

    Share project "Lesson04HomeTask04" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson12" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson10HomeTask04" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson06HomeTask04_1" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson07" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson10HomeTask05" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson05HomeTask14" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson12HomeTask01" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson10" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson09" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson08HomeTask03" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson06HomeTask04_2" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson02" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson08HomeTask02" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson03HomeTask04" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson08HomeTask01" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson03" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson09HomeTask_1" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson10HomeTask01" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson06" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson10HomeTask02" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson05" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson02HomeTask04" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson01HomeTask04" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson07HomeTask04" into "https://casterprojects.googlecode.com/svn/JavaSE" Share project "Lesson10HomeTask03" into "https://casterprojects.googlecode.com/svn/JavaSE"

---
+++ JavaSE/trunk/Lesson01HomeTask04/src/Lesson01Task04.java
@@ -0,0 +1,19 @@
+
+public class Lesson01Task04 {
+	public static void main(String[] args) 
+	{
+		System.out.println("Список арифметических операций:\n");
+		System.out.println("+\tоперация сложения");
+		System.out.println("-\tоперация вычитания");
+		System.out.println("*\tоперация умножения");
+		System.out.println("/\tоперация деления");
+		System.out.println("%\tоперация деления по модулю");
+		System.out.println("++\tоперация инкрементации");
+		System.out.println("--\tоперация декрементации");			
+		System.out.println("+=\tоперация сложения с присвоением");
+		System.out.println("-=\tоперация вычитания с присвоением");
+		System.out.println("*=\tоперация умножения с присвоением");
+		System.out.println("/=\tоперация деления с присвоением");
+		System.out.println("%=\tоперация деления по модулю с присвоением");
+	}
+}
