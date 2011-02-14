3e4f54ffec9fd0417472ff818cd10d4735f04bd6
vlad_dracula@tut.by <vlad_dracula@tut.by@0ce95480-0193-c130-4e2c-61057acf6c8b>
2011-02-14 19:15:59 +0000

    Начальный импорт файлов проекта OA4LogClient в репозиторий.

---
+++ OldDelphiCode/XE/trunk/OA4LogClient/OA4LogClient.dpr
@@ -0,0 +1,24 @@
+﻿program OA4LogClient;
+
+{$R *.dres}
+
+uses
+  Forms,
+  Windows,
+  Main in 'Main.pas' {MainForm},
+  About in 'About.pas' {AboutForm};
+
+{$R *.res}
+begin
+	Application.Initialize;
+	if (FindWindow('TMainForm','OA4 Log Client')=0) and (FindWindow('TMainForm','OA4')<>0) then
+		begin
+      //Application.MainFormOnTaskbar:=True;
+			Application.Title:='OA4 Log Client';
+      Application.HelpFile:='';
+      Application.ShowMainForm:=False;
+      Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+    end
+  else Application.Terminate;
+end.
