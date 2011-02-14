e819bef697d98fa85f3349e886ce3e962a49a189
vlad_dracula@tut.by <vlad_dracula@tut.by@0ce95480-0193-c130-4e2c-61057acf6c8b>
2011-02-14 21:24:06 +0000

    Начальный импорт проектов, написанных под BDS2009 (последняя версия BDS без юникода по умолчанию).

---
+++ OldDelphiCode/BDS2009/trunk/GAIDBLogClient/About.pas
@@ -0,0 +1,59 @@
+unit About;
+
+interface
+
+uses
+  Windows, Messages, SysUtils, Variants, Classes, Graphics, Controls, Forms,
+  Dialogs, ExtCtrls, StdCtrls, ShellAPI, Main;
+
+type
+  TAboutForm = class(TForm)
+    Timer1: TTimer;
+    Shape1: TShape;
+    Image1: TImage;
+    Label1: TLabel;
+    Label2: TLabel;
+    Label3: TLabel;
+    Label5: TLabel;
+    Button1: TButton;
+    Timer2: TTimer;
+    Label4: TLabel;
+    procedure Timer2Timer(Sender: TObject);
+    procedure Label5Click(Sender: TObject);
+    procedure Button1Click(Sender: TObject);
+    procedure Timer1Timer(Sender: TObject);
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  AboutForm: TAboutForm;
+
+implementation
+
+{$R *.dfm}
+
+procedure TAboutForm.Timer1Timer(Sender: TObject);
+begin
+	ModalResult:=mrOk;
+end;
+
+procedure TAboutForm.Button1Click(Sender: TObject);
+begin
+  ModalResult:=mrOk;
+end;
+
+procedure TAboutForm.Label5Click(Sender: TObject);
+begin
+  ShellExecute(Application.Handle,'open',PChar('mailto:kuzmichIV@yandex.ru'),nil,nil,SW_MAXIMIZE);
+end;
+
+procedure TAboutForm.Timer2Timer(Sender: TObject);
+begin
+	if ((AlphaBlendValue+17)<=222) then
+  	AlphaBlendValue:=AlphaBlendValue+17;
+end;
+
+end.
