956d4a84dac6035c90483fe3b9afff0690396ffa
vlad_dracula@tut.by <caster1977@yandex.ru>
2011-02-14 21:24:06 +0000

    Начальный импорт проектов, написанных под BDS2009 (последняя версия BDS без юникода по умолчанию).

---
+++ OldDelphiCode/BDS2009/trunk/GAIDBLogClient/GAIDBLogClient.dpr
@@ -0,0 +1,21 @@
+﻿program GAIDBLogClient;
+
+uses
+	Forms,
+	Windows,
+	Main in 'Main.pas' {MainForm},
+	About in 'About.pas' {AboutForm};
+
+{$R *.res}
+begin
+	Application.Initialize;
+	if (FindWindow('TMainForm','GAIDB Log Client')=0) and (FindWindow('TMainForm','GAIDB')<>0) then
+		begin
+			Application.Title:='GAIDB Log Client';
+      Application.HelpFile:='';
+      Application.ShowMainForm:=False;
+      Application.CreateForm(TMainForm, MainForm);
+			Application.Run;
+    end
+  else Application.Terminate;
+end.
