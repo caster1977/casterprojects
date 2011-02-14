a4ff425333433ea3ce5ea8a95b917f1e0a6bd6b8
vlad_dracula@tut.by <caster1977@yandex.ru>
2011-02-14 20:31:17 +0000

    Начальный импорт файлов проекта TestSysMenu в репозиторий.

---
+++ OldDelphiCode/XE/trunk/TestSysMenu/TestSysMenu.dpr
@@ -0,0 +1,13 @@
+program TestSysMenu;
+
+uses
+  Forms,
+  Main in 'Main.pas' {MainForm};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TMainForm, MainForm);
+  Application.Run;
+end.
