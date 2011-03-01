c9eb516bf3df925e15b882ec67fc777fb2046537
vlad_dracula@tut.by <vlad_dracula@tut.by@0ce95480-0193-c130-4e2c-61057acf6c8b>
2011-03-01 08:34:45 +0000

    Подготовка запросов на выгрузку АСКР за март 2011 года.

--- Other/trunk/statserver/c/mysql/ASKR_01_10.bat
+++ Other/trunk/statserver/c/mysql/ASKR_01_10.bat
@@ -1,3 +1,3 @@
-md D:\ASKR\20110201_10
-c:\mysql\bin\mysql -u ASKR --password=ASKR < ASKR20110201_10.sql
-copy D:\ASKR\20110201_10\summary_top.txt /B + D:\ASKR\20110201_10\summary_bottom.txt /B D:\ASKR\20110201_10\summary.txt
+md D:\ASKR\20110301_10
+c:\mysql\bin\mysql -u ASKR --password=ASKR < ASKR20110301_10.sql
+copy D:\ASKR\20110301_10\summary_top.txt /B + D:\ASKR\20110301_10\summary_bottom.txt /B D:\ASKR\20110301_10\summary.txt
--- Other/trunk/statserver/c/mysql/ASKR_21_31.bat
+++ Other/trunk/statserver/c/mysql/ASKR_21_31.bat
@@ -1,3 +1,3 @@
-md D:\ASKR\20110221_28
-c:\mysql\bin\mysql -u ASKR --password=ASKR < ASKR20110221_28.sql
-copy D:\ASKR\20110221_28\summary_top.txt /B + D:\ASKR\20110221_28\summary_bottom.txt /B D:\ASKR\20110221_28\summary.txt
+md D:\ASKR\20110321_31
+c:\mysql\bin\mysql -u ASKR --password=ASKR < ASKR20110321_31.sql
+copy D:\ASKR\20110321_31\summary_top.txt /B + D:\ASKR\20110321_31\summary_bottom.txt /B D:\ASKR\20110321_31\summary.txt
