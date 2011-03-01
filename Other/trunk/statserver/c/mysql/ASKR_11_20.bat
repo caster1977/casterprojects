45246a52c8b63859bcc69dd4657d472a0641003a
vlad_dracula@tut.by <caster1977@yandex.ru>
2011-03-01 08:34:45 +0000

    Подготовка запросов на выгрузку АСКР за март 2011 года.

--- Other/trunk/statserver/c/mysql/ASKR_11_20.bat
+++ Other/trunk/statserver/c/mysql/ASKR_11_20.bat
@@ -1,3 +1,3 @@
-md D:\ASKR\20110211_20
-c:\mysql\bin\mysql -u ASKR --password=ASKR < ASKR20110211_20.sql
-copy D:\ASKR\20110211_20\summary_top.txt /B + D:\ASKR\20110211_20\summary_bottom.txt /B D:\ASKR\20110211_20\summary.txt
+md D:\ASKR\20110311_20
+c:\mysql\bin\mysql -u ASKR --password=ASKR < ASKR20110311_20.sql
+copy D:\ASKR\20110311_20\summary_top.txt /B + D:\ASKR\20110311_20\summary_bottom.txt /B D:\ASKR\20110311_20\summary.txt
