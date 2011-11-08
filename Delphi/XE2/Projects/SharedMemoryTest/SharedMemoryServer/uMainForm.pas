7f4c1e995727bc20e2967379e8c5bd7178bbfc11
vlad_dracula@tut.by <vlad_dracula@tut.by@0ce95480-0193-c130-4e2c-61057acf6c8b>
2011-11-08 14:19:22 +0000

    Добавление тестового проекта

---
+++ Delphi/XE2/Projects/SharedMemoryTest/SharedMemoryServer/uMainForm.pas
@@ -0,0 +1,34 @@
+unit uMainForm;
+
+interface
+
+uses
+  Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ActnList, Vcl.Menus, Vcl.ImgList,
+  Vcl.PlatformDefaultStyleActnCtrls, Vcl.ActnMan, Vcl.ComCtrls;
+
+type
+  TMainForm = class(TForm)
+    MainMenu1: TMainMenu;
+    ActionManager1: TActionManager;
+    ImageList1: TImageList;
+    Action_Quit: TAction;
+    Action_About: TAction;
+    Action_Configuration: TAction;
+    N1: TMenuItem;
+    N2: TMenuItem;
+    N3: TMenuItem;
+    N4: TMenuItem;
+    N5: TMenuItem;
+    N6: TMenuItem;
+    StatusBar1: TStatusBar;
+  end;
+
+var
+  MainForm: TMainForm;
+
+implementation
+
+{$R *.dfm}
+
+end.
